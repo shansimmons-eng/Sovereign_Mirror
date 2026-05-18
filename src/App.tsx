efbc009ba595849eb432d6f68bc25c63185a2f43
shansimmons-eng <shan.simmons@gmail.com>
2026-05-18 01:26:20 -0500

    Restore TestDashboard - drives live feed events to VeracityLog via syncVeracityToLedger

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Dashboard } from './components/ui/Dashboard';
 import { Background } from './components/ui/Background';
+import { TestDashboard } from './components/ui/TestDashboard';
 
 export function App() {
   return (
     <div className="relative min-h-screen h-screen bg-void-black overflow-hidden">
       <Background />
+      <TestDashboard />
       <Dashboard />
     </div>
   );
