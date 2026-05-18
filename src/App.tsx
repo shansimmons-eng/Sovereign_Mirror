6541ee35728c69d021dc62fa36dd7abfc2f064b5
shansimmons-eng <shan.simmons@gmail.com>
2026-05-18 01:10:31 -0500

    Fix vertical scroll - remove TestDashboard stacking, Background already fixed-position

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import { Dashboard } from './components/ui/Dashboard';
 import { Background } from './components/ui/Background';
-import { TestDashboard } from './components/ui/TestDashboard';
 
 export function App() {
   return (
-    <div className="relative min-h-screen bg-void-black overflow-hidden">
+    <div className="relative min-h-screen h-screen bg-void-black overflow-hidden">
       <Background />
-      <TestDashboard />
       <Dashboard />
     </div>
   );
